25110e365a1bb621fffa7915e4133c16b64e0556
johnnywo <office@zeero.at>
2018-02-28 13:50:21 +0100

    res.form

--- plugins/milo/reservation/components/ReservationForm.php
+++ plugins/milo/reservation/components/ReservationForm.php
@@ -88,8 +88,9 @@ class ReservationForm extends ComponentBase
 	            Mail::queue('milo.reservation::mail.message', $vars, function($message) use ($vars) {
 
 	            	$message->from($vars['email'], $vars['name']);
-	            	$message->to('1060@laurel-leaf.at', 'Laurel Leaf Irish Pub');
-	            	$message->cc('laurelleaf1060@gmail.com', 'Laurel Leaf GMail');
+	            	$message->to('emil.esletzbichler@icloud.com', 'Emil');
+	            	//$message->to('1060@laurel-leaf.at', 'Laurel Leaf Irish Pub');
+	            	//$message->cc('laurelleaf1060@gmail.com', 'Laurel Leaf GMail');
 	            	$message->bcc($vars['email'], $vars['name']);
 	            	$message->subject('Reservierungsanfrage (auto-reply)');
 	            });
