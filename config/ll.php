e9f651a4e990b44e22900898923dab6db2a1b8a3
johnnywo <office@zeero.at>
2019-11-21 14:27:43 +0100

    winners 10tages geburtstagsfeier

---
+++ config/ll.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+	'winners'	=> [
+		[
+			'name'	=> 'Stefan R.',
+			'prize'	=> '100,- € Gutschein',
+		],
+		[
+			'name'	=> 'Martin V.',
+			'prize'	=> '50,- € Gutschein',
+		],
+		[
+			'name'	=> 'Verona',
+			'prize'	=> 'JBL Bluetooth Lautsprecher',
+		],
+		[
+			'name'	=> 'Felix B.',
+			'prize'	=> 'Heineken Rucksack',
+		],
+		[
+			'name'	=> 'Bettina H.',
+			'prize'	=> 'Desperados Tasche',
+		],
+		[
+			'name'	=> 'Mod',
+			'prize'	=> 'Strongbox Liegestuhl',
+		],
+		[
+			'name'	=> 'Barbara P.',
+			'prize'	=> 'Strongbox Liegestuhl',
+		],
+		[
+			'name'	=> 'Michael H.',
+			'prize'	=> 'Strongbox Luftmatratze',
+		],		[
+			'name'	=> 'Lorenz S.',
+			'prize'	=> 'Strongbox Luftmatratze',
+		],		[
+			'name'	=> 'Lukii E.',
+			'prize'	=> '2 Pitscher Gläser',
+		],
+
+	]
+
+
+];
